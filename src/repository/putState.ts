b0fbbb7685290c70b3925a57d10427308548e4ad
c1322368 Raiuri Santos de Sousa Pinto <raiuri.pinto@capgemini.com>
2022-07-14 19:59:10 -0300

    refactoring

--- src/repository/putState.ts
+++ src/repository/putState.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { RobotState } from "../types";
 
-export const putState = async (state: RobotState) => {
+export const putState = async (state: RobotState):Promise<RobotState> => {
     
     const { data } = await axios.put('http://localhost:3000/robotState/1/', state );
 
--- src/services/afterPlacedRobotService.ts
+++ src/services/afterPlacedRobotService.ts
@@ -1,29 +1,32 @@
 import { afterPlacedRobot } from "../partials/prompts";
 import axios from "axios";
 import { moveRobotService } from "./moveRobotService";
-import { ROBOT_STATE } from "../constants";
+import { ROBOT_STATE, AFTER_PLACE_COMMANDS } from "../constants";
 import { Service } from "./types";
+import { putState } from "../repository/putState";
 
 export const afterPlacedRobotService: Service = (prevState, restartGame) => {
     afterPlacedRobot().then(async (state) => {
         switch (state.commandAfter) {
-            case 'move': {
+            
+            case AFTER_PLACE_COMMANDS.MOVE: {
                 moveRobotService(prevState, restartGame);
                 break;
             }
 
-            case 'reset': {
-                await axios.put('http://localhost:3000/robotState/1', { ...ROBOT_STATE, placed: false });
+            case AFTER_PLACE_COMMANDS.RESET: {
+                await putState({ ...ROBOT_STATE, placed: false });
                 restartGame();
                 break;
             }
 
-            case 'exit': {
+            case AFTER_PLACE_COMMANDS.EXIT: {
                 return;
             }
 
             default: {
                 console.log('invalid command, Valid commands are: move | reset | exit');
+                
                 restartGame();
             }
         }
